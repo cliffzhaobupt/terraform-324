56a76bbd905b796be49179aac9f1b5586d5a83a9
趙 天宇 <s04122@ca-20018534.cyberagent.co.jp>
2023-04-03 17:50:56 +0900

    feat: init commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "fluid-brook-379710"
+  region  = "asia-east1"
+  zone    = "asia-east1-b"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
